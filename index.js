c914186f8a09946bdda455e6574db8a6a61ee9ab
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-03 08:26:34 -0500

    refacror code, used forEach instaed of loop

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ tweetBtn.addEventListener('click', function(){
 })
 function getFeedHtml(){
     let feedHtml = ``
-    for (let tweet of tweetsData){
+    tweetsData.forEach((tweet) => {
         feedHtml += `<div class="tweet">
         <div class="tweet-inner">
             <img src="${tweet.profilePic}" class="profile-pic">
@@ -27,8 +27,7 @@ function getFeedHtml(){
             </div>            
         </div>
     </div>`
-    
-    }
+    })
     return feedHtml
 }
 
