39b9519b5a242ab6b715c01311824fe58f6fefa4
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-06 14:10:41 -0500

    added ability to increment/decrement retweets

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ document.addEventListener("click", (e)=> {
     if(e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
     }
+    else if (e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+        
+    }
 })
 function handleLikeClick(tweetId){
     const targetTweetObj = tweetsData.filter(function(tweet){
@@ -23,6 +27,21 @@ function handleLikeClick(tweetId){
     targetTweetObj.isLiked = !targetTweetObj.isLiked
     render()
 }
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter((tweet)=> {
+        return tweet.uuid === tweetId
+    })[0]
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets --
+    }
+    else {
+        targetTweetObj.retweets ++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
+
 function getFeedHtml(){
     let feedHtml = ``
     tweetsData.forEach((tweet) => {
